10dcdcbb9e98e3e44ba717b4529fef96b238e364
Siju Sakarias <sijus887@gmail.com>
2024-04-20 17:27:33 -0500

    Remove unused theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,6 @@ import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
 
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
 require("prismjs/themes/prism-tomorrow.css");
+// Highlighting for code blocks
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
