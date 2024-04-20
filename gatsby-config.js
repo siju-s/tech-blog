6429126a97d9bca46235f24db702b2e8107f54d1
Siju Sakarias <sijus887@gmail.com>
2024-04-20 16:38:23 -0500

    Update author info and pic

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,11 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Siju Sakaria`,
+      summary: `- Senior Mobile developer`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
   },
   plugins: [
     `gatsby-plugin-image`,
